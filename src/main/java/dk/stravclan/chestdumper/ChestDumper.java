ba297571c5f61f9a6b70553e3cb8d91ccb6ad0ba
Kris Kruse <kriskruse@hotmail.com>
2022-12-30 13:05:12 +0100

    Update ChestDumper.java

--- src/main/java/dk/stravclan/chestdumper/ChestDumper.java
+++ src/main/java/dk/stravclan/chestdumper/ChestDumper.java
@@ -18,7 +18,7 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("chestdumper")
 public class ChestDumper
 {
     // Directly reference a slf4j logger
